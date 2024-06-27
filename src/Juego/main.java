7cf2a96c8a9c10b014861c8685c5d13610f47c54
Gianella Halan <gianellahalan@gmail.com>
2024-06-27 16:25:02 -0300

    version 2

--- src/Juego/main.java
+++ src/Juego/main.java
@@ -1,10 +1,61 @@
 package Juego;
 
+import java.util.Scanner;
+
 public class main {
 
+	static Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// generar el núm aleatorio
+		int aleatorio = 0;
+		aleatorio = (int) (Math.random() * 10 + 1);
+		System.out.println(aleatorio);
+		int intentos=0;
+		int numElegido;
+		
+		do {
+			
+			//ingresar núm por teclado
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Elija un número del 1 al 10");
+			numElegido = scanner.nextInt();
+			generarLineaLarga();
+			intentos++;//contador
+			
+			//comparar núm aleatorio con el núm ingresado
+			if (aleatorio == numElegido) {
+				System.out.println("Ha ganado! El número era " + aleatorio);
+				generarLineaLarga();
+				scanner.close();
+			}
+			else {
+				//si el número ingresado es menor al aleatorio
+				if (numElegido<aleatorio) {
+					System.out.println("Número demasiado bajo");
+					generarLineaLarga();
+				}else{
+					//si el número ingresado es mayor al aleatorio
+					if(numElegido>aleatorio) {
+						System.out.println("Número demasiado alto");
+						generarLineaLarga();}
+				       }
+				}
+		}//cierra do
+		
+		//hacer que se repita la operación
+		while(intentos <5 & numElegido != aleatorio); //cierra do while
+		if (numElegido != aleatorio) {
+			System.out.println("Ha perdido! El número era " + aleatorio);
+			generarLineaLarga();
+		}
+		scanner.close();	
+		System.out.println("FIN DEL JUEGO");
+		
+	}//cierra public static
 
-	}
-
-}
+	public static void generarLineaLarga() {
+		System.out.println("________________________________\n");
+		}
+}//cierra class main
